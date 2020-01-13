c11a75d9886a8f4621c85a30629aca218bb15a31
Nakamoto-art <bclarence747@gmail.com>
2020-01-13 21:26:05 +0900

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  module ChatSpace
+    class Application < Rails::Application
+      config.generators do |g|
+        g.stylesheets false
+        g.javascripts false
+        g.helper false
+        g.test_framework false
+      end
+    end
+  end
+end
